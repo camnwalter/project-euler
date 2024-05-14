bd15f54742419c6b5b3b5deacf0b43ff77d83a95
Cam Walter <cam.n.walter@gmail.com>
2024-05-14 02:05:41 -0500

    8
    Signed-off-by: Cam Walter <cam.n.walter@gmail.com>

---
+++ problems/8.go
@@ -0,0 +1,42 @@
+package problems
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Eight() {
+
+	bytes, err := os.ReadFile("inputs/8.txt")
+	if err != nil {
+		fmt.Println("error reading input")
+		return
+	}
+
+	contents := strings.ReplaceAll(string(bytes), "\n", "")
+
+	largestProduct := 0
+
+outer:
+	for i := 0; i < len(contents)-13; i++ {
+		product := 1
+
+		for j := i; j < i+13; j++ {
+			num := int(contents[j] - '0')
+
+			if num == 0 {
+				i = j
+				continue outer
+			}
+
+			product *= num
+		}
+
+		if product > largestProduct {
+			largestProduct = product
+		}
+	}
+
+	fmt.Println("largest product =", largestProduct)
+}
