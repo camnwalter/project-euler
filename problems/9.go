3b4775c04ad97437b284836e3b4f7cf7ba8c2b33
Cam Walter <cam.n.walter@gmail.com>
2024-05-14 02:20:17 -0500

    9
    Signed-off-by: Cam Walter <cam.n.walter@gmail.com>

---
+++ problems/9.go
@@ -0,0 +1,23 @@
+package problems
+
+import "fmt"
+
+func Nine() {
+	var a int
+	var b int
+	var c int
+
+outer:
+	for a = 1; a < 1000; a++ {
+		for b = 1; b < 1000; b++ {
+			for c = 1; c < 1000; c++ {
+				if a*a+b*b == c*c && a+b+c == 1000 {
+					break outer
+				}
+			}
+		}
+	}
+
+	fmt.Println("abc =", a*b*c)
+
+}
