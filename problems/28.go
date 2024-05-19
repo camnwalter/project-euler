034d5cb1886028f9a500d4fe891dd55d93bd8ef6
Cam Walter <cam.n.walter@gmail.com>
2024-05-19 01:07:10 -0500

    28
    Signed-off-by: Cam Walter <cam.n.walter@gmail.com>

---
+++ problems/28.go
@@ -0,0 +1,33 @@
+package problems
+
+import "fmt"
+
+func TwentyEight() {
+	// 3, 13, 31 => +2, +10, +18
+	// 5, 17, 37 => +4, +12, +20
+	// 7, 21, 43 => +6, +14, +22
+	// 9, 25, 49 => +8, +16, +24
+
+	a, b, c, d := 1, 1, 1, 1
+	n := 2
+
+	sum := 1
+
+	for step := 1; step*2+1 <= 1001; step++ {
+		a += n
+		n += 2
+
+		b += n
+		n += 2
+
+		c += n
+		n += 2
+
+		d += n
+		n += 2
+
+		sum += a + b + c + d
+	}
+
+	fmt.Println("sum =", sum)
+}
--- problems/problem.go
+++ problems/problem.go
@@ -7,7 +7,7 @@ func Problem(num int) {
 		One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Eleven,
 		Twelve, Thirteen, Fourteen, Fifteen, Sixteen, Seventeen, Eighteen,
 		Nineteen, Twenty, TwentyOne, TwentyTwo, TwentyThree, TwentyFour,
-		TwentyFive, TwentySix, TwentySeven,
+		TwentyFive, TwentySix, TwentySeven, TwentyEight,
 	}
 
 	if num == 0 {
