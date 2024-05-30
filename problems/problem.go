0cd16cc7cae69dfb3b17f9e3aa8c71b71458132c
Cam Walter <cam.n.walter@gmail.com>
2024-05-30 13:08:53 -0500

    63 lol
    Signed-off-by: Cam Walter <cam.n.walter@gmail.com>

--- problems/problem.go
+++ problems/problem.go
@@ -20,6 +20,7 @@ func Problem(num int) {
 		FortyThree, FortyFour, FortyFive, FortySix, FortySeven, FortyEight,
 		FortyNine, Fifty, FiftyOne, FiftyTwo, FiftyThree, FiftyFour, FiftyFive,
 		FiftySix, FiftySeven, FiftyEight, FiftyNine, Sixty, nil, SixtyTwo,
+		SixtyThree,
 	}
 
 	answers, _ := utils.GetFileLines("answers.txt")
